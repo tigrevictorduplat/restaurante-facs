e80e33022d0793844d333646247073bd6a220c70
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2025-05-28 14:25:36 -0300

    Correções de Identação

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 @app.route('/reservas', methods=['POST'])
 def criar_reserva():
     dados = request.json
+
     def operacao(cursor):
-        sql="""
+        sql = """
             INSERT INTO tb_reservas 
             (dataReserva, horaReserva, nomeCliente, quatidadePessoas, statusReservaConfirmada, idMesaReserva)
             VALUES (%s, %s, %s, %s, %s, %s)
@@ -26,35 +27,33 @@ def criar_reserva():
 
     reserva_id = conectar_banco(operacao)
 
-    return jsonify({'mensagem': 'Reserva criada', 'idReserva': reserva_id})        
-
-
-
+    return jsonify({'mensagem': 'Reserva criada', 'idReserva': reserva_id})
 
 
 @app.route('/mesas', methods=['GET'])
 def listar_mesas():
     def operacao(cursor):
-        cursor.execute("SELECT* FROM tb_mesas")
+        cursor.execute("SELECT * FROM tb_mesas")
         return cursor.fetchall()
+
     dados = conectar_banco(operacao)
 
     mesas = []
     for linha in dados:
         mesas.append({
-            'idMesas':linha[0],
-            'statusMesaOcupada':linha[1]
+            'idMesas': linha[0],
+            'statusMesaOcupada': linha[1]
         })
-        return jsonify(mesas)
-    
+    return jsonify(mesas)
 
-    @app.route('/reservas/<int: idReserva>', methods=['DELETE'])
-    def deletar_reserva(idReserva):
-        def operacao(cursor):
-            cursor.execute("DELETE FROM tb_reservas WHERE idReserva = %s", (idReserva,))
 
-            conectar_banco(operacao)
+@app.route('/reservas/<int:idReserva>', methods=['DELETE'])
+def deletar_reserva(idReserva):
+    def operacao(cursor):
+        cursor.execute("DELETE FROM tb_reservas WHERE idReserva = %s", (idReserva,))
+    conectar_banco(operacao)
+    return jsonify({'mensagem': 'Reserva deletada com sucesso'})
 
-            return jsonify({'mensagem': 'Reserva deletada com sucesso'})
-        
-        app.run()
+
+if __name__ == '__main__':
+    app.run(debug=True)
